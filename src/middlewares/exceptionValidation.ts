f39cfedf78eaebc38375fc5267bef95da211a6df
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-05-03 20:50:08 -0300

    refactor exceptionValidation middleware to return array of errors

--- src/middlewares/exceptionValidation.ts
+++ src/middlewares/exceptionValidation.ts
@@ -7,7 +7,7 @@ import { ZodError } from 'zod'
 export const exceptionValidation: ErrorRequestHandler = async (err, req, res, next) => {
   if (err instanceof ZodError) {
     return res.status(400).json({
-      error: `${err.errors[0].path} ${await translateText(err.errors[0].message)}`
+      errors: err.errors.map(async error => `${error.path} ${await translateText(error.message)}`)
     })
   }
 
