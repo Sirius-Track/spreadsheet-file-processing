e02e5df94f059d40edff1431d23cfae01b609149
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-05-03 21:30:02 -0300

    refactor spreadSheet function to use request body

--- src/modules/spread-sheet/index.ts
+++ src/modules/spread-sheet/index.ts
@@ -7,8 +7,7 @@ import { SpreadSheetSchema } from './validation/validation'
 export const spreadSheed = async (data: SpreadSheet) => {
   const { dataUrl, userId, projectId, sendTo } = SpreadSheetSchema.parse(data)
 
-  console.log({ dataUrl, userId, projectId, sendTo })
-  console.log()
+  const BATCH_SIZE = 2000
 
   const fileCSV = await fetch(dataUrl)
 
@@ -29,11 +28,10 @@ export const spreadSheed = async (data: SpreadSheet) => {
 
   // Mantenha o cabeçalho para uso nas partes divididas
   const header = Object.keys(records.data[0])
-  console.log(header)
 
-  /* for (let i = 0; i < records.length; i += BATCH_SIZE) {
-    const slice = records.slice(i, i + BATCH_SIZE)
-    const csvChunk = stringify([header, ...slice.map(row => header.map(field => row[field]))])
+  for (let i = 0; i < records.data.length; i += BATCH_SIZE) {
+    const slice = records.data.slice(i, i + BATCH_SIZE)
+    const csvChunk = JSON.stringify([header, ...slice.map(row => header.map(field => row[field]))])
 
     // Envie o chunk para a rota 'postCSV'
     await fetch('https://<your-supabase-url>/rest/v1/postCSV', {
@@ -41,5 +39,5 @@ export const spreadSheed = async (data: SpreadSheet) => {
       headers: { 'Content-Type': 'text/csv' },
       body: csvChunk
     })
-  } */
+  }
 }
