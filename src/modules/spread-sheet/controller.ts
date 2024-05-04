87a767ac24eb935d16637e1ca6b04d70fe48a17d
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-05-03 21:46:11 -0300

    refactor spreadSheet function to use request body

--- src/modules/spread-sheet/controller.ts
+++ src/modules/spread-sheet/controller.ts
@@ -2,8 +2,8 @@ import { spreadSheed } from '.'
 
 import type { RequestHandler } from 'express'
 
-export const createspreadSheed: RequestHandler = async (req, res) => {
-  await spreadSheed(req.body)
+export const createspreadSheed: RequestHandler = (req, res) => {
+  spreadSheed(req.body)
 
   res.status(201).send({
     message: 'Seu arquivo está sendo processado com sucesso'
