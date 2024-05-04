6b49c88eb45f29e016c42832e1cec349b32443b7
DIEGOHORVATTI <72998710+DIEGOHORVATTI@users.noreply.github.com>
2024-05-04 03:56:58 +0000

    refactor spreadSheet function to format date and time values consistently

--- src/modules/spread-sheet/index.ts
+++ src/modules/spread-sheet/index.ts
@@ -36,17 +36,7 @@ export const spreadSheed = async (data: SpreadSheet) => {
     skipEmptyLines: true
   })
 
-  // Mantenha o cabeçalho para uso nas partes divididas
-  const header = Object.keys(records.data[0])
-
-  console.log(records.data[0])
-
-  const timef = '02/09/2024 01:02:20'
-  const par = moment(timef, 'DD/MM/YYYY HH:mm:ss').format('YYYY-MM-DD HH:mm:ss')
-
-  console.log(par)
-
-  /* const formattedRows: Array<RowData> = records.data.map(row => {
+  const formattedRows: Array<RowData> = records.data.map(row => {
     const formattedRow: {
       [key: string]: string
       user_id: string
@@ -57,16 +47,7 @@ export const spreadSheed = async (data: SpreadSheet) => {
     }
 
     for (const [header, value] of Object.entries(row)) {
-      console.log('-------')
-      console.log(header.toLocaleLowerCase())
-      console.log('=======')
-
       if (['data da transação', 'confirmação do pagamento'].includes(header.toLocaleLowerCase())) {
-        'data da transação' === header.toLocaleLowerCase() && console.log('iquais')
-        console.log(value)
-
-        'confirmação do pagamento' === header.toLocaleLowerCase() && console.log('iquais')
-
         formattedRow[header] = moment(value, 'DD/MM/YYYY HH:mm:ss').format('YYYY-MM-DD HH:mm:ss')
       } else {
         formattedRow[header] = moment(value, 'DD/MM/YYYY HH:mm:ss').format('YYYY-MM-DD HH:mm:ss').trim()
@@ -80,13 +61,16 @@ export const spreadSheed = async (data: SpreadSheet) => {
     return formattedRow
   })
 
-  console.log(formattedRows[0]) */
+  // Mantenha o cabeçalho para uso nas partes divididas
+  const header = Object.keys(formattedRows[0])
+
+  console.log(formattedRows[0])
 
   const database = 'sales_duplicate'
 
   // Divida os registros em partes de tamanho fixo de 2mil e envie-os para a rota 'postCSV' do Supabase
-  for (let i = 0; i < records.data.length; i += BATCH_SIZE) {
-    const slice = records.data.slice(i, i + BATCH_SIZE)
+  for (let i = 0; i < formattedRows.length; i += BATCH_SIZE) {
+    const slice = formattedRows.slice(i, i + BATCH_SIZE)
     const csvChunk = JSON.stringify([header, ...slice.map(row => header.map(field => row[field]))])
 
     // chuck de envio
