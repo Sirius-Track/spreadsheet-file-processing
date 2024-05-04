88932a8ef71fbc7c31f115eddff9e13d7e3c5c72
Diogo <diogojorge1401@gmail.com>
2024-05-04 09:53:16 -0300

    Update controller.ts
    @DIEGOHORVATTI  gay tem que usar await pra promessa

--- src/modules/spread-sheet/controller.ts
+++ src/modules/spread-sheet/controller.ts
@@ -3,7 +3,7 @@ import { spreadSheed } from '.'
 import type { RequestHandler } from 'express'
 
 export const createspreadSheed: RequestHandler = (req, res) => {
-  spreadSheed(req.body)
+  await spreadSheed(req.body)
 
   res.status(201).send({
     message: 'Your file is being processed successfully'
