5ff0361a43126ed8e3666bbf634a4f0929bb638f
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-05-03 21:10:11 -0300

    refactor exceptionValidation middleware to return array of errors

--- src/middlewares/exceptionValidation.ts
+++ src/middlewares/exceptionValidation.ts
@@ -4,6 +4,22 @@ import { translateText } from '@/shared'
 
 import { ZodError } from 'zod'
 
+const error = async (err: any) => {
+  if (typeof err.message === 'string') {
+    return [await translateText(err.message)]
+  }
+
+  if (err.message instanceof Array) {
+    return err.message.map(async (error: string) => await translateText(error))
+  }
+
+  if (err.message instanceof Object) {
+    return Object.values(err.message).map(async (error: any) => await translateText(error))
+  }
+
+  return [translateText('An error occurred')]
+}
+
 export const exceptionValidation: ErrorRequestHandler = async (err, req, res, next) => {
   if (err instanceof ZodError) {
     return res.status(400).json({
@@ -11,23 +27,7 @@ export const exceptionValidation: ErrorRequestHandler = async (err, req, res, ne
     })
   }
 
-  const error = async () => {
-    if (typeof err.message === 'string') {
-      return [await translateText(err.message)]
-    }
-
-    if (err.message instanceof Array) {
-      return err.message.map(async (error: string) => await translateText(error))
-    }
-
-    if (err.message instanceof Object) {
-      return Object.values(err.message).map(async (error: any) => await translateText(error))
-    }
-
-    return [translateText('An error occurred')]
-  }
-
   return res.status(500).json({
-    errors: await error()
+    errors: await error(err)
   })
 }
