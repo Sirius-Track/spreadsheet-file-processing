2aec32ad56ac9963200afce5fcff0c3e5aaf1fa3
DIEGOHORVATTI <72998710+DIEGOHORVATTI@users.noreply.github.com>
2024-05-06 06:55:15 +0000

    refact: Update getFormatedValue to use simplified date formatting

--- src/modules/spread-sheet/shared/functions/getFormatedValue.ts
+++ src/modules/spread-sheet/shared/functions/getFormatedValue.ts
@@ -5,11 +5,25 @@ type Props = {
   value: string
 }
 
+const identifyDateFormat = (value: string): string => {
+  if (/^\d{4}-\d{2}-\d{2}/.test(value)) {
+    return 'YYYY-MM-DD HH:mm:ss'
+  }
+
+  if (/^\d{2}-\d{2}-\d{4}/.test(value)) {
+    return 'DD-MM-YYYY HH:mm:ss'
+  }
+
+  return value
+}
+
 export const getFormatedValue = ({ isFormatted, value }: Props) => {
   if (isFormatted) {
-    const dateFormated = dayjs(value).format('YYYY-MM-DD')
+    const format = identifyDateFormat(value)
 
-    return dateFormated
+    const dateFormatted = dayjs(value, format).format('YYYY-MM-DD')
+
+    return dateFormatted
   }
 
   return value.trim()
