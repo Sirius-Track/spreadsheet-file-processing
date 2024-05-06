00881b325c36e63f45367ecf54eedfc0bd65c856
DIEGOHORVATTI <72998710+DIEGOHORVATTI@users.noreply.github.com>
2024-05-06 07:07:33 +0000

    refact: Update hotmartHeader to use generic types for header values
    This commit updates the `hotmartHeader` object in the `spread-sheet/shared/headers/hotmartHeader.ts` file to use generic types for the header values. Previously, the object was using specific types for each header value, but now it uses a generic type (`HeadersValues<PerfectpayHeaderValues>`) that can be used for any header values. This change improves code reusability and flexibility.

--- src/modules/spread-sheet/shared/headers/hotmartHeader.ts
+++ src/modules/spread-sheet/shared/headers/hotmartHeader.ts
@@ -1,4 +1,37 @@
-export const hotmartHeader: { [key: string]: string } = {
+import { HeadersValues } from './types'
+
+export type PerfectpayHeaderValues = {
+  transaction_code: string
+  transaction_status: string
+  transaction_date: string
+  producer: string
+  product_id: string
+  product_name: string
+  offer_id: string
+  offer_name: string
+  currency: string
+  purchase_value_with_tax: string
+  purchase_value_without_tax: string
+  commission_currency: string
+  my_commission_value: string
+  src_code: string
+  sck_code: string
+  payment_method: string
+  total_installments: string
+  total_charges: string
+  coupon_code: string
+  buyer_name: string
+  buyer_email: string
+  buyer_country: string
+  buyer_phone: string
+  buyer_document: string
+  buyer_state: string
+  buyer_instagram: string
+  order_bump_type: string
+  order_bump_transaction: string
+}
+
+export const hotmartHeader: HeadersValues<PerfectpayHeaderValues> = {
   'Código da transação': 'transaction_code',
   'Status da transação': 'transaction_status',
   'Data da transação': 'transaction_date',
