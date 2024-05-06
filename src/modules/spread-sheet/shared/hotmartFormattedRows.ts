a529e38139e11f9182bbe689733f5f865e48a0fa
DIEGOHORVATTI <72998710+DIEGOHORVATTI@users.noreply.github.com>
2024-05-06 00:14:03 +0000

    refact: improve code modularity and readability in hotmartFormattedRows.ts
    This commit refactors the `hotmartFormattedRows.ts` file to improve code modularity and readability. It introduces a separate function named `getFormatedValue` to handle date formatting logic, making the main function cleaner and easier to maintain.

--- src/modules/spread-sheet/shared/hotmartFormattedRows.ts
+++ src/modules/spread-sheet/shared/hotmartFormattedRows.ts
@@ -22,7 +22,7 @@ export const hotmartFormattedRows = ({ records, platform, user_id, project_id }:
     for (const [header, value] of Object.entries({ ...row, ...formattedRow })) {
       const mappedHeader = hotmartHeader[header]
 
-      const isFormatted = mappedHeader && ['transaction_date'].includes(mappedHeader.toLowerCase())
+      const isFormatted = Boolean(mappedHeader && ['transaction_date'].includes(mappedHeader.toLowerCase()))
 
       if (mappedHeader) {
         formattedRow[mappedHeader] = getFormatedValue({ isFormatted, value })
