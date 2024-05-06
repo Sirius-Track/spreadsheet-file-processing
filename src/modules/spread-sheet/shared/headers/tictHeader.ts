61324e3e4fbd59ec38c03098c229c67384a2c5e5
DIEGOHORVATTI <72998710+DIEGOHORVATTI@users.noreply.github.com>
2024-05-06 04:44:32 +0000

    refact: Update tictHeader to use generic type for header values
    This commit updates the `tictHeader` object in the `spread-sheet/shared/headers/tictHeader.ts` file to use a generic type for the header values. Previously, the object was using a specific type (`TictHeaderValues`) for the header values, but now it uses a generic type (`HeadersValues`) that can be used for any header values. This change improves code reusability and flexibility.

--- src/modules/spread-sheet/shared/headers/tictHeader.ts
+++ src/modules/spread-sheet/shared/headers/tictHeader.ts
@@ -1,4 +1,32 @@
-export const tictHeader: { [key: string]: string | null } = {
+import { genHash } from '../'
+
+import type { Missing } from '../types'
+import type { HeadersValues } from './types'
+
+export type TictHeaderValues = {
+  transaction_code: string
+  transaction_status: string
+  transaction_date: string
+  product_name: string
+  offer_id: string
+  offer_name: string
+  purchase_value_with_tax: string
+  purchase_value_without_tax: string
+  my_commission_value: string
+  src_code: string
+  sck_code: string
+  payment_method: string
+  total_installments: string
+  coupon_code: string
+  buyer_name: string
+  buyer_email: string
+  buyer_country: string
+  buyer_phone: string
+  buyer_document: string
+  buyer_state: string
+}
+
+export const tictHeader: HeadersValues<TictHeaderValues> = {
   'Código da Transação': 'transaction_code',
   Status: 'transaction_status',
   'Data do Pedido': 'transaction_date',
@@ -21,13 +49,16 @@ export const tictHeader: { [key: string]: string | null } = {
   Estado: 'buyer_state'
 }
 
-export const tictMissing: { [key: string]: string | null } = {
-  producer: null, // "Não fornecido pela plataforma."
-  product_id: null, // genHash(product_name)
-  currency: null,
-  commission_currency: null, // "Não fornecido pela plataforma."
-  total_charges: null, // "Não fornecido pela plataforma."
-  buyer_instagram: null, // "Não fornecido pela plataforma."
-  order_bump_type: null, // "Não fornecido pela plataforma."
-  order_bump_transaction: null // "Não fornecido pela plataforma."
+export const tictMissing = (row: Missing<TictHeaderValues>) => {
+  return {
+    ...row,
+    product_id: genHash(row.product_name), // genHash(product_name)
+    producer: 'undefined', // "Não fornecido pela plataforma."
+    currency: 'undefined',
+    commission_currency: 'undefined', // "Não fornecido pela plataforma."
+    total_charges: 'undefined', // "Não fornecido pela plataforma."
+    buyer_instagram: 'undefined', // "Não fornecido pela plataforma."
+    order_bump_type: 'undefined', // "Não fornecido pela plataforma."
+    order_bump_transaction: 'undefined' // "Não fornecido pela plataforma."
+  }
 }
