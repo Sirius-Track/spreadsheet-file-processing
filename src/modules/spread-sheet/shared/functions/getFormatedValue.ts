05d9be0bd8b1d7248119cef3f5ced8bae121013c
DIEGOHORVATTI <72998710+DIEGOHORVATTI@users.noreply.github.com>
2024-05-06 07:34:49 +0000

    refact: Update getFormatedValue to support additional date formats
    This commit updates the `getFormatedValue` function in the `spread-sheet/shared/functions/getFormatedValue.ts` file to support additional date formats. It adds support for the format 'YYYY/MM/DD HH:mm:ss' and 'DD-MM-YYYY HH:mm:ss'. This change improves the flexibility of the function by allowing it to handle a wider range of date formats.

--- src/modules/spread-sheet/shared/functions/getFormatedValue.ts
+++ src/modules/spread-sheet/shared/functions/getFormatedValue.ts
@@ -10,8 +10,20 @@ const identifyDateFormat = (value: string): string => {
     return 'YYYY-MM-DD HH:mm:ss'
   }
 
+  if (/^\d{4}\/\d{2}\/\d{2}/.test(value)) {
+    return 'YYYY/MM/DD HH:mm:ss'
+  }
+
   if (/^\d{2}-\d{2}-\d{4}/.test(value)) {
     return 'DD-MM-YYYY HH:mm:ss'
+  }
+
+  if (/^\d{2}-\d{2}-\d{4}/.test(value)) {
+    return 'DD-MM-YYYY HH:mm:ss'
+  }
+
+  if (/^\d{2}\/\d{2}\/\d{4}/.test(value)) {
+    return 'DD/MM/YYYY HH:mm:ss'
   }
 
   return value
