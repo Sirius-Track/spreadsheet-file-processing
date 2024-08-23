aaa66bf744ca874cf628c4b5dbc6fc47f81bc193
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-08-23 09:15:12 -0300

    feat: Add optional mask fields to SpreadSheetSchema
    This commit adds optional mask fields to the SpreadSheetSchema, allowing for more flexibility in masking specific transaction data in the spreadsheet.

--- src/modules/spread-sheet/validation/SpreadSheetSchema.ts
+++ src/modules/spread-sheet/validation/SpreadSheetSchema.ts
@@ -1,7 +1,5 @@
 import * as z from 'zod'
 
-import type { SpreadSheet } from '../types'
-
 export const platforms = [
   'custom',
   'hotmart',
@@ -27,7 +25,35 @@ export const SpreadSheetSchema = z.object({
     return value
   }),
   userId: z.string(),
-  projectId: z.string()
+  projectId: z.string(),
+  maskTransactionCode: z.string().optional(),
+  maskTransactionStatus: z.string().optional(),
+  maskTransactionDate: z.string().optional(),
+  maskProducer: z.string().optional(),
+  maskProductId: z.string().optional(),
+  maskProductName: z.string().optional(),
+  maskOfferId: z.string().optional(),
+  maskOfferName: z.string().optional(),
+  maskCurrency: z.string().optional(),
+  maskPurchaseValueWithTax: z.string().optional(),
+  maskPurchaseValueWithoutTax: z.string().optional(),
+  maskCommissionCurrency: z.string().optional(),
+  maskMyCommissionValue: z.string().optional(),
+  maskSrcCode: z.string().optional(),
+  maskSckCode: z.string().optional(),
+  maskPaymentMethod: z.string().optional(),
+  maskTotalInstallments: z.string().optional(),
+  maskTotalCharges: z.string().optional(),
+  maskCouponCode: z.string().optional(),
+  maskBuyerName: z.string().optional(),
+  maskBuyerEmail: z.string().optional(),
+  maskBuyerCountry: z.string().optional(),
+  maskBuyerPhone: z.string().optional(),
+  maskBuyerDocument: z.string().optional(),
+  maskBuyerState: z.string().optional(),
+  maskBuyerInstagram: z.string().optional(),
+  maskOrderBumpType: z.string().optional(),
+  maskOrderBumpTransaction: z.string().optional()
 })
 
 export type SpreadSheetInput = z.infer<typeof SpreadSheetSchema>
