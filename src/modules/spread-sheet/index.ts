76adbeec9f540a45817e5661fccf0ad9b3af87b1
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-09-23 12:42:28 -0300

    feat: Add more descriptive console log in spreadSheet function

--- src/modules/spread-sheet/index.ts
+++ src/modules/spread-sheet/index.ts
@@ -7,7 +7,7 @@ import type { SpreadSheet } from './types'
 export const spreadSheed = async (data: SpreadSheet) => {
   const { dataUrl, userId, platform, projectId, ...rest } = SpreadSheetSchema.parse(data)
 
-  console.log(data)
+  console.log('KAPA:', data)
 
   const fileCSV = await fetch(dataUrl)
 
