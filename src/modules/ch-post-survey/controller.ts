e6a0ac6ae232748f003f5543602760ce2e93ffc5
paulotfchaves <43656788+paulotfchaves@users.noreply.github.com>
2024-09-26 08:14:02 -0300

    bug: remove res.status200 from controller

--- src/modules/ch-post-survey/controller.ts
+++ src/modules/ch-post-survey/controller.ts
@@ -7,11 +7,6 @@ export const chPostSurvey: RequestHandler = async (req, res) => {
   try {
     // Validar e responder antes de processar
     await surveySheet(req.body, res)
-
-    // Responde ao frontend antes de iniciar o processamento em segundo plano
-    res.status(200).send({
-      message: 'CSV validado com sucesso, o processamento será feito em segundo plano.'
-    })
   } catch (erro: any) {
     res.status(400).send({
       message: erro.message || 'Erro ao processar o arquivo de pesquisa'
