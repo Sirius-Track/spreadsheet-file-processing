6fd91bb9287d8c1a61e073d9f4ea7f66c7b4c0cb
paulotfchaves <43656788+paulotfchaves@users.noreply.github.com>
2024-10-09 18:05:52 -0300

    chore: console.log

--- src/modules/spread-sheet/controller.ts
+++ src/modules/spread-sheet/controller.ts
@@ -23,7 +23,7 @@ export const SheetSchema = z.object<SchemaRequiredZod<Pick<SpreadSheet, 'dataUrl
 })
 
 export const createspreadSheed: RequestHandler = async (req, res) => {
-  console.log('Request Body:', JSON.stringify(req.body, null, 2))
+  console.log('Request Body:', JSON.stringify(req.body))
   const { dataUrl, platform } = SheetSchema.parse(req.body)
 
   try {
